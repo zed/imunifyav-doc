3d40b7d1cba9882466261ce7f55b707719814b72
Anuar Tastembekov <tastembekov.anuar@gmail.com>
2019-02-13 17:00:11 +0600

    HELP-5 #comment ImunifyAV improvements

--- docs/.vuepress/urls-mapping.js
+++ docs/.vuepress/urls-mapping.js
@@ -1,3 +1,5 @@
 module.exports = {
-  
+    "https://docs.imunifyav.com/index.html?installation_guide.htm": "/imunifyav/#installation-guide",
+    "https://docs.imunifyav.com/index.html?installation_guide2.htm": "/imunifyav/#installation-guide",
+    "https://docs.imunifyav.com/index.html?imunifyav2.htm": "/imunifyav/"
 };
