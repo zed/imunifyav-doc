7a03d32f28393d7b545c97640da2764189c78aa0
Anuar Tastembekov <tastembekov.anuar@gmail.com>
2019-02-13 20:14:26 +0600

    Fix urls mappings list

--- docs/.vuepress/urls-mapping.js
+++ docs/.vuepress/urls-mapping.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "https://docs.imunifyav.com/index.html?installation_guide.htm": "/imunifyav/#installation-guide",
-    "https://docs.imunifyav.com/index.html?installation_guide2.htm": "/imunifyav/#installation-guide",
-    "https://docs.imunifyav.com/index.html?imunifyav2.htm": "/imunifyav/"
+    "/index.html?installation_guide.htm": "/imunifyav/#installation-guide",
+    "/index.html?installation_guide2.htm": "/imunifyav/#installation-guide",
+    "/index.html?imunifyav2.htm": "/imunifyav/"
 };
