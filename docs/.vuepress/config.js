3ced96f1a4d9c4844be3f2cdd9952af904742969
Azamat <sarsenbayev.a.s@gmail.com>
2020-05-12 13:37:40 +0600

    HELP-73 Install two plugins into vuepress docs (#18)
    * Upgrade vuepress version
    
    * Add disqus and google analytics
    
    * Upgrade theme
    
    Co-authored-by: Azamat Sarsenbayev <asarsenbayev@cloudlinux.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,24 @@
 const urls = require("./urls-mapping.js");
 
 module.exports = {
+  plugins: [
+    ['container', {
+      type: 'warning',
+      before: info => `<div class="warning custom-block"><p class="custom-block-title">${info}</p>`,
+      after: '</div>',
+    }],
+    ['container', {
+      type: 'tip',
+      before: info => `<div class="tip custom-block"><p class="custom-block-title">${info}</p>`,
+      after: '</div>',
+    }],
+    ['disqus', { shortname: 'cldocs' }],
+    ['@vuepress/google-analytics',
+      {
+        'ga': 'UA-12711721-12'
+      }
+    ]
+  ],
   configureWebpack: {
     resolve: {
       alias: {
