4865472ea2ddee6edb661f2c9223b505c7b9270c
Azamat <sarsenbayev.a.s@gmail.com>
2020-05-14 17:37:20 +0600

    HELP-74 Investigate why redirects do not work (#19)
    * Upgrade theme
    
    * Fix danger tip
    
    Co-authored-by: Azamat Sarsenbayev <asarsenbayev@cloudlinux.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,6 +12,11 @@ module.exports = {
       before: info => `<div class="tip custom-block"><p class="custom-block-title">${info}</p>`,
       after: '</div>',
     }],
+    ['container', {
+      type: 'danger',
+      before: info => `<div class="danger custom-block"><p class="custom-block-title">${info}</p>`,
+      after: '</div>',
+    }],
     ['disqus', { shortname: 'cldocs' }],
     ['@vuepress/google-analytics',
       {
